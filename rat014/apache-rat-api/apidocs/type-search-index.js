ffc2d90987e18359348fcf10f4f0988e9b3715b1
Hugo Hirsch <github@hugo-hirsch.de>
2022-05-29 13:05:18 +0200

    Push new preview version of RAT 0.14

---
+++ rat014/apache-rat-api/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"org.apache.rat.api.domain","l":"LicenseFamily"},{"p":"org.apache.rat.api.domain","l":"LicenseFamilyBuilder"},{"p":"org.apache.rat.api.domain","l":"RatLicenseFamily"}];updateSearchResults();
