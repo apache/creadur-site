9b72f435f2d30167cd387188e9b0c0f15bfe5e63
Hugo Hirsch <github@hugo-hirsch.de>
2022-10-23 22:20:58 +0200

    Update WHISKER subpage

---
+++ whisker/apache-whisker-maven-plugin/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.apache.creadur.whisker.plugin.maven","l":"GenerateMojo"},{"p":"org.apache.creadur.whisker.plugin.maven","l":"HelpMojo"},{"p":"org.apache.creadur.whisker.plugin.maven","l":"MojoToJCLLog"}];updateSearchResults();
---
+++ whisker/apache-whisker-scan/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.apache.creadur.whisker.scan","l":"Directory"},{"p":"org.apache.creadur.whisker.scan","l":"FromFileSystem"}];updateSearchResults();
