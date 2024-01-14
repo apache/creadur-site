4e03c554f46b7272db1d36c9c2e438d4d06332ef
Hugo Hirsch <github@hugo-hirsch.de>
2024-01-14 21:27:39 +0100

    Cleanup to get in changes from RAT-346

--- rat0161/apache-rat-api/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"org.apache.rat.api.domain","l":"LicenseFamily"},{"p":"org.apache.rat.api.domain","l":"LicenseFamilyBuilder"},{"p":"org.apache.rat.api.domain","l":"RatLicenseFamily"}];updateSearchResults();
