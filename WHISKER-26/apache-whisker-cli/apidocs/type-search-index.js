6c4288e6426e4a0a556ca4cbcb26633e22351adb
P. Ottlinger <pottlinger@apache.org>
2024-12-13 22:06:01 +0100

    WHISKER-26: Cleanup

--- WHISKER-26/apache-whisker-cli/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.apache.creadur.whisker.cli","l":"CommandLineOption"},{"p":"org.apache.creadur.whisker.cli","l":"Main"},{"p":"org.apache.creadur.whisker.cli","l":"SystemLog"}];updateSearchResults();
