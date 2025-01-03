b7829e46935faecfdbbc7d1ec47279b6b546fb99
P. Ottlinger <pottlinger@apache.org>
2025-01-03 23:00:45 +0100

    Cleanup tentacles subdir

--- tentacles/testapidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.apache.creadur.tentacles","l":"ArchivesJsonTest"},{"p":"org.apache.creadur.tentacles.filter","l":"ListOfFilesFilterTest"}];updateSearchResults();
