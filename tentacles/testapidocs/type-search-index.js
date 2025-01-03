d4c4397da408d936189fc9cf7c2bd34d2d93d900
P. Ottlinger <pottlinger@apache.org>
2025-01-03 23:03:14 +0100

    Regen tentacles page after changes cleanup

---
+++ tentacles/testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.apache.creadur.tentacles","l":"ArchivesJsonTest"},{"p":"org.apache.creadur.tentacles.filter","l":"ListOfFilesFilterTest"}];updateSearchResults();
