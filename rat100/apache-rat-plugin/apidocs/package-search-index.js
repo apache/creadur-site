ec3f0e155fefec487ceffb31faa18db3304a2f7a
P. Ottlinger <pottlinger@apache.org>
2026-01-25 16:45:36 +0100

    Regen all webpages with corrected download links

--- rat100/apache-rat-plugin/apidocs/package-search-index.js
+++ rat100/apache-rat-plugin/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.rat.mp"},{"l":"org.apache.rat.plugin"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.apache.rat.mp"},{"l":"org.apache.rat.plugin"}];updateSearchResults();
--- whisker/apache-whisker-xml/apidocs/package-search-index.js
+++ whisker/apache-whisker-xml/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.creadur.whisker.fromxml"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.apache.creadur.whisker.fromxml"}];updateSearchResults();
