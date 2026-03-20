7bb5a6cdd9b617c51739a8d1dcfdd9d2e23674da
P. Ottlinger <pottlinger@apache.org>
2026-03-20 15:38:17 +0100

    RAT-530: Promote r0.18

--- rat/apache-rat-tools/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.rat.documentation"},{"l":"org.apache.rat.documentation.options"},{"l":"org.apache.rat.documentation.velocity"},{"l":"org.apache.rat.tools"},{"l":"org.apache.rat.tools.xsd"}];updateSearchResults();
