bb07c23d0a17f02be238d6c3e33d367ea8a08b69
P. Ottlinger <pottlinger@apache.org>
2026-05-25 23:41:41 +0200

    Add new WHISKER homepage

--- whisker/apache-whisker-scan/apidocs/type-search-index.js
+++ whisker/apache-whisker-scan/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"org.apache.creadur.whisker.scan","l":"Directory"},{"p":"org.apache.creadur.whisker.scan","l":"FromFileSystem"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.apache.creadur.whisker.scan","l":"Directory"},{"p":"org.apache.creadur.whisker.scan","l":"FromFileSystem"}];updateSearchResults();
