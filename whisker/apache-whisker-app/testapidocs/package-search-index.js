15e65486dc7c16fb8b63573a88f3fa6cb76512d3
P. Ottlinger <pottlinger@apache.org>
2026-07-04 18:31:45 +0200

    WHISKER-21: Regen site with sonar and jacoco

--- whisker/apache-whisker-app/testapidocs/package-search-index.js
+++ whisker/apache-whisker-app/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.creadur.whisker.app"},{"l":"org.apache.creadur.whisker.app.load"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.apache.creadur.whisker.app"},{"l":"org.apache.creadur.whisker.app.load"}];updateSearchResults();
